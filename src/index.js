2526ddad9eca598cb11d8d434f57aac0323a654e
Feixuan Li <feixuanl@usc.edu>
2018-12-29 02:21:17 +0800

    position get

--- src/index.js
+++ src/index.js
@@ -1,13 +1,37 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const App = () => {
-    window.navigator.geolocation.getCurrentPosition(
-        (position) => {console.log(position)},
-        (err) => {console.log(err)}
-    );
-    return <div> hi there</div>
-};
+class App extends React.Component {
+    // specifically to js , not must-have 
+    // called any time the instance of this class is created
+    constructor(props) { // automatically called with props object
+        super(props); // must-have , a referrence to the parent's constructor function
+        
+        // initialize state 
+        this.state = {
+            lat: null,
+            errMessage: '',
+        };
+
+        window.navigator.geolocation.getCurrentPosition(
+            (position) => {
+                this.setState({lat: position.coords.latitude})
+            },
+            (err) => {
+                this.setState({errMessage: err.message})
+            }
+        );
+    }
+    // specifically to react, required
+    render() {
+        if(this.state.errMessage) {
+            return <div>Error: {this.state.errMessage}</div>;
+        } else if (this.state.lat) {
+            return <div> Latitude: {this.state.lat} </div>;
+        }
+        return <div>loading</div>;
+    }
+}
 
 ReactDOM.render(
     <App />,
